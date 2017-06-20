c42334e0a7e9b069a411ab61129a932a4919e7fe
Adam Snow <adamjohnsnow@icloud.com>
2017-06-20 13:17:44 +0100

    Multiple images isolated tests

--- assets/passphrase.js
+++ assets/passphrase.js
@@ -1,3 +1,14 @@
-var returned = 'anthony'
-
-module.exports = returned;
+var imageArray = [
+  "IMG_0773.jpg",
+  "IMG_0774.jpg",
+  "IMG_0775.jpg",
+  "IMG_0776.jpg",
+  "IMG_0777.jpg",
+  "IMG_0778.jpg",
+  "IMG_0779.jpg"
+]
+var randImage = function(number){
+  var imageIndex = number || Math.floor(Math.random() * imageArray.length)
+  return [imageArray[imageIndex], imageIndex]
+}
+module.exports = randImage;
