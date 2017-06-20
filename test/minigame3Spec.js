85677080795996ca224c582707decb20b84ce019
JayWebDevCom <jaiye.s@gmail.com>
2017-06-20 17:58:17 +0100

    passing zombie test for image counting and attribute assertion

--- test/minigame3Spec.js
+++ test/minigame3Spec.js
@@ -6,21 +6,18 @@ var app = require('../index.js')
 describe('minigame3', function(){
   const browser = new Browser();
 
-  it('the route \/minigame3 should exist', function(done){
-    browser.visit('http://localhost:8080/minigame3').then(function(){
-      browser.assert.success()
-    }).then(done, done)
+  before(function(done) {
+    browser.visit('http://localhost:8080/minigame3', done);
+  });
+
+  it('should display an image with id mainImage', function(){
+    browser.assert.success()
+    browser.assert.element('img#mainImage')
   })
 
-  it('should display an image with id #mainImage', function(done){
-    browser.visit('http://localhost:8080/minigame3').then(function(){
-      browser.assert.className('img', 'mainImage')
-    }).then(done, done)
+  it('should display 3 images on the page with id testImage', function(){
+    browser.assert.success()
+    browser.assert.elements('img.testImage', 3)
   });
 
-  it('should display 4 images on the page', function(done){
-    browser.visit('http://localhost:8080/minigame3').then(function(){
-      browser.assert.elements('img', 4)
-    }).then(done, done)
-  });
 });
