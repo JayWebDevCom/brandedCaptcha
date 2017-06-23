b51115d74bd090fcf261d2af65e7c47fe63f3c96
Ben Taylor <taylor.ben89@gmail.com>
2017-06-23 16:34:06 +0100

    name load modal function as captchup

--- captchupFiles/loadModal.js
+++ captchupFiles/loadModal.js
@@ -1,12 +1,14 @@
-$( function() {
-  $( "#dialog-message" ).dialog({
+var captchup = function(){
+  $( function() {
+  $("#dialog-message").dialog({
     modal: true,
-
     width: 'auto',
     closeOnEscape: false,
     resizable: false,
     maxHeight: 800,
     position: {my: "center", at: "top", of: window }
-
   });
-} );
+});
+}
+
+captchup()
