312f67ad9bc4ea03824269a793f83dbd334527ef
hendrik <g9a2hvk9@me.com>
2017-06-26 12:15:40 +0100

    Refactors index and minigames

--- assets/imgAssoc.js
+++ assets/imgAssoc.js
@@ -2,86 +2,47 @@ var mains = [{id:300, img:'cisse'}, {id:301, img:'ronaldo'}, {id:302, img:'murra
 var solutions = [{id:300, img:'adidasBoots'}, {id:301, img:'nikeBoots'}, {id:302, img:'headRacket'}, {id:303, img:'rangeRoverCar'}, {id:304, img:'lufthansaLogo'}]
 var decoys = [{id:000, img:'dog'}, {id:001, img:'duck'}, {id:002, img:'mug'}, {id:003, img:'pen'}, {id:004, img:'plate'}]
 
-var imgAssoc = function(){
+var imgAssoc = function(stubNumber){
+  var randomIndex = stubNumber || Math.floor(Math.random() * mains.length)
+  var mainImage = mains[randomIndex]
+
   this.type = 'imgAssoc'
-  this.randomIndex = Math.floor(Math.random() * mains.length)
-
-  this.promptArray = this.getPromptArray(this.getMainImage(this.randomIndex).id);
-  this.promptStrings = this.buildPromptStrings(this.promptArray);
-
-  this.gameKey = this.getMainImage(this.randomIndex).id
+  this.gameKey = mainImage.id
+  this.promptStrings = this.getPromptArray(mainImage.id)
 
   this.gameData = {
-    mainString: this.buildMainImageString(this.getMainImage(this.randomIndex)),
+    mainString: mainImage.img,
     promptStrings: this.promptStrings
   };
 };
 
-imgAssoc.prototype.getGameData = function () {
-  return this.gameData
-}
-
-
-imgAssoc.prototype.getMainImage = function(index){
-  return mains[index]
-};
-
-
-imgAssoc.prototype.buildMainImageString = function(mainImage){
-  return mainImage.img
-};
-
-
-imgAssoc.prototype.getSolution = function(mainImageid){
-
-  function findSolution(hash) {
-    return hash.id === mainImageid;
-  };
-
-  return solutions.find(findSolution);
-
-};
-
-
 imgAssoc.prototype.getPromptArray = function(mainImageid){
-
   function shuffle(array) {
     for (let i = array.length; i; i--) {
         let j = Math.floor(Math.random() * i);
         [array[i - 1], array[j]] = [array[j], array[i - 1]];
     };
   };
-
   var promptArray = [];
-
-  for(i = 0; i<2; i++){ promptArray.push(decoys[Math.floor(Math.random() * decoys.length)])};
-  promptArray.push(this.getSolution(mainImageid));
-
+  for(i = 0; i<2; i++){ promptArray.push(decoys[Math.floor(Math.random() * decoys.length)].img)};
+  promptArray.push(this.getSolution(mainImageid).img);
   shuffle(promptArray);
   return promptArray;
 };
 
-imgAssoc.prototype.buildPromptStrings = function(array){
-  promptStrings = []
-  array.forEach(function(entry){
-    promptStrings.push(entry.img)
-  });
-  return promptStrings
+imgAssoc.prototype.getSolution = function(mainImageid){
+  function findSolution(hash) {
+    return hash.id === mainImageid;
+  };
+  return solutions.find(findSolution);
 };
 
 imgAssoc.prototype.checkAnswer = function(gameKey, name){
-
   function findImage(hash) {
     return gameKey === hash.id;
   };
-
   var returnedImage = solutions.find(findImage);
-
   return returnedImage.img === name
-}
-
-imgAssoc.prototype.testFunction = function(index){
-  console.log('this is a test')
 }
 
 module.exports = imgAssoc;
